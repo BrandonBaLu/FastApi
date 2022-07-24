4ce60dc0d0b90d75e9c7692533f1b5193159bc44
Brandon Balderas Lucas <patolucas.bbl@otlook.com>
2022-07-23 21:45:59 -0500

    listo

--- git/backend/main.py
+++ git/backend/main.py
@@ -116,8 +116,14 @@ async def create_user(usuario: UserIN ):
         user = auth.create_user_with_email_and_password(usuario.email, usuario.password)
         uid = user["localId"]
         db.child("users").child(uid).set({"email": usuario.email, "level": 1 })
-        message = {"Usuario Agregado"}
-        return message
+        response = {"Usuario Agregado"}
+        return response
     except Exception as error:
         print(f"Error: {error}")
+        return(f"Error: {error}")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Client exist",
+        )
+        
         
